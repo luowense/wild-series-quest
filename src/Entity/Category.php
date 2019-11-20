bb81e8cab2b1590812cede36bcdac0d8ed9ede8f
Laurent Segura <54800835+luowense@users.noreply.github.com>
2019-11-20 17:37:03 +0100

    enf of symfony quest 6

---
+++ src/Entity/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\String_;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+
+}
